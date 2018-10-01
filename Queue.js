b63ee7833026177a09e7e163bb42765708b59f71
husseyexplores <husseyexplores@gmail.com>
2018-10-01 23:41:44 +0500

    Add Queue data structure

---
+++ Queue.js
@@ -0,0 +1,14 @@
+/* Queues  */
+
+function Queue() {
+  collection = [];
+  
+  this.print = function() {
+    console.log(collection);
+  }
+
+  this.enqueue = function(element) {
+    collection.push(element);
+  }
+
+}
