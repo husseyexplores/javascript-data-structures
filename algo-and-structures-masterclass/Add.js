39ff4c0b37a08c61944b97353535850dba849e30
husseyexplores <husseyexplores@gmail.com>
2018-10-17 16:07:22 +0500

    Solve addUpTo with O(n) and O(1)

---
+++ algo-and-structures-masterclass/Add.js
@@ -0,0 +1,24 @@
+// TC => O(n)   -   SC => O(1)
+const addUpTo1 = n => {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+let t1 = performance.now();
+console.log(addUpTo1(1000000000));
+let t2 = performance.now();
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`);
+
+/* ********* ********** */
+
+// TC => O(1)   -   SC => O(1)
+const addUpTo2 = n => {
+  return n * (n + 1) / 2;
+}
+t1 = performance.now();
+console.log(addUpTo2(1000000000));
+t2 = performance.now();
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`);
