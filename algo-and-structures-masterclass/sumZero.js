2860a83331f4ed56261e62a5328785d2538a62ae
husseyexplores <husseyexplores@gmail.com>
2018-10-17 22:03:30 +0500

    Add sumZero solution using lr pointers

---
+++ algo-and-structures-masterclass/sumZero.js
@@ -0,0 +1,25 @@
+// pointers - left and right
+
+// provided array is already sorted
+// Time complexity O(n) - Space complexity O(1);
+
+const sumZero = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return undefined;
+}
+
+sumZero([-3, -2, -1, 0, 1, 2, 3]); // [-3, 3]
+sumZero([-2, 0, 1, 3]); // undefined
+sumZero([1, 2, 3]); // undefined
