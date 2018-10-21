4cc655191897578edc4c2821431f0771ae29ea88
husseyexplores <husseyexplores@gmail.com>
2018-10-21 20:59:20 +0500

    Binary seach example

---
+++ algo-and-structures-masterclass/Search_Binary.js
@@ -0,0 +1,19 @@
+// only works for sorted arrays
+const binarySeach = (arr, val) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== val && start <= end) {
+    if (val > arr[middle]) {
+      start = middle + 1;
+    } else if (val < arr[middle]) {
+      end = middle - 1;
+    }
+    middle = Math.floor(middle / 2);
+  }
+  if (arr[middle] === val) return middle;
+  return -1;
+};
+
+binarySeach([8, 16, 24, 32, 40, 48, 56, 64, 72, 80], 32);
