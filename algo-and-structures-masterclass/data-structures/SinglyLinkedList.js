05d25053dfa6cdb05174aede668f9c104d26a6ed
husseyexplores <husseyexplores@gmail.com>
2018-10-28 20:20:23 +0500

    Add singlyLinkedList push method

---
+++ algo-and-structures-masterclass/data-structures/SinglyLinkedList.js
@@ -0,0 +1,29 @@
+// Singly Linked List
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
