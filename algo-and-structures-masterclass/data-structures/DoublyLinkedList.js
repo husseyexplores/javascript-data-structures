6b9952daeed8f08748b42b63814ea513e320209f
husseyexplores <husseyexplores@gmail.com>
2018-10-30 10:52:38 +0500

    Define push method for DoublyLinkedList

---
+++ algo-and-structures-masterclass/data-structures/DoublyLinkedList.js
@@ -0,0 +1,39 @@
+// Doubly Linked List
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push('Hi!');
+list.push('Good morning.');
+list.push('Good afternoon');
+list.push('Good evening');
+list.push('Good night.');
+list.push('Bye!');
