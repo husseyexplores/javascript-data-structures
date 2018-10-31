a1b58b9e1e01a03c0acce4450864da5baf0c9735
husseyexplores <husseyexplores@gmail.com>
2018-10-31 17:57:19 +0500

    Define unshift method for DoublyLinkedList

--- algo-and-structures-masterclass/data-structures/DoublyLinkedList.js
+++ algo-and-structures-masterclass/data-structures/DoublyLinkedList.js
@@ -58,6 +58,20 @@ class DoublyLinkedList {
     this.length--;
     return headToRemove;
   }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const list = new DoublyLinkedList();
