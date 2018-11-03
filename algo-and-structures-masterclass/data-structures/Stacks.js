0129d49b515311c04d3db5c06ac9f3de96703034
husseyexplores <husseyexplores@gmail.com>
2018-11-03 16:48:53 +0500

    Add Stack data structure

--- algo-and-structures-masterclass/data-structures/Stacks.js
+++ algo-and-structures-masterclass/data-structures/Stacks.js
@@ -1,1 +1,40 @@
 // Stacks
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const oldFirst = this.first;
+      this.first = newNode;
+      this.first.next = oldFirst;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    const nodeToRemove = this.first;
+    this.first = nodeToRemove.next;
+    this.size--;
+    if (this.size === 0) {
+      this.last = null;
+    }
+    return nodeToRemove.val;
+  }
+}
