5f1b9de92a74a5deeab7813a6edfca86dd619816
husseyexplores <husseyexplores@gmail.com>
2018-11-04 23:49:55 +0500

    Define insert method for BST

--- algo-and-structures-masterclass/data-structures/BinarySearchTree.js
+++ algo-and-structures-masterclass/data-structures/BinarySearchTree.js
@@ -1,8 +1,8 @@
 class Node {
   constructor(val) {
     this.val = val;
-    this.right = null;
     this.left = null;
+    this.right = null;
   }
 }
 
@@ -10,4 +10,32 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let currentNode = this.root;
+    while (true) {
+      if (val === currentNode.val) return undefined;
+      if (val > currentNode.val) {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      } else if (val < currentNode.val) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      }
+    }
+  }
 }
+
+const tree = new BinarySearchTree();
