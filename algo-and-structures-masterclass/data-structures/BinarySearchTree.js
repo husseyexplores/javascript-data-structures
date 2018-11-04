bcf0bfb2e2aae2767fd7e0958dfd746c8ba16d16
husseyexplores <husseyexplores@gmail.com>
2018-11-04 20:08:55 +0500

    Initialize BST

---
+++ algo-and-structures-masterclass/data-structures/BinarySearchTree.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
