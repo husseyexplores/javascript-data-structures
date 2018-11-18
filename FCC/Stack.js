d282cd930bb291f1c054e1de35247b5205370c67
husseyexplores <husseyexplores@gmail.com>
2018-11-18 09:40:28 +0500

    Eslint fixes

--- FCC/Stack.js
+++ FCC/Stack.js
@@ -1,41 +1,40 @@
 // Creates a stack
 
-var Stack = function () {
+const Stack = function() {
   this.count = 0;
   this.storage = {};
 
   // Add a value to the stack
-  this.push = function (value) {
+  this.push = function(value) {
     this.storage[this.count] = value;
     this.count++;
-  }
+  };
 
-  //Remove a value from the stack
-  this.pop = function () {
+  // Remove a value from the stack
+  this.pop = function() {
     if (this.count === 0) {
       return undefined;
     }
 
     this.count--;
-    var result = this.storage[this.count];
+    const result = this.storage[this.count];
     delete this.storage[this.count];
     return result;
-  }
+  };
 
-  this.size = function () {
+  this.size = function() {
     return this.count;
-  }
+  };
 
   // Returns the value at the end of the stack (last pushed)
-  this.peek = function () {
+  this.peek = function() {
     return this.storage[this.count - 1];
-  }
-
+  };
 };
 
 // Example Usage
 
-var myStack = new Stack();
+const myStack = new Stack();
 myStack.push('Hi - 1');
 myStack.push('Hi - 2');
 myStack.push('Hi - 3');
