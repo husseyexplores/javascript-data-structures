06120e782e18b967471650d24201ae63980f1681
husseyexplores <husseyexplores@gmail.com>
2018-11-18 09:38:46 +0500

    Eslint fixes

--- FCC/PriorityQueue.js
+++ FCC/PriorityQueue.js
@@ -1,20 +1,21 @@
 /* Priority Queues  */
 
 function PriorityQueue() {
-  collection = [];
+  const collection = [];
 
-  this.print = function () {
+  this.print = function() {
     console.log(collection);
-  }
+  };
 
-  this.enqueue = function (element) {
+  this.enqueue = function(element) {
     if (this.isEmpty()) {
-      collection.push(element)
+      collection.push(element);
     } else {
       let added = false;
 
       for (let i = 0; i < collection.length; i++) {
-        if (element[1] < collection[i][1]) { // checking priorities
+        // checking priorities
+        if (element[1] < collection[i][1]) {
           collection.splice(i, 0, element);
           added = true;
           break;
@@ -25,23 +26,23 @@ function PriorityQueue() {
         collection.push(element);
       }
     }
-  }
+  };
 
-  this.dequeue = function () {
+  this.dequeue = function() {
     return collection.shift()[0];
-  }
+  };
 
-  this.front = function () {
+  this.front = function() {
     return collection[0];
-  }
+  };
 
-  this.size = function () {
+  this.size = function() {
     return collection.length;
-  }
+  };
 
-  this.isEmpty = function () {
-    return (collection.lengtn === 0);
-  }
+  this.isEmpty = function() {
+    return collection.lengtn === 0;
+  };
 }
 
 // Examples
