6fb0c937965865d4f902bb958eba9498706cd8d5
husseyexplores <husseyexplores@gmail.com>
2018-11-18 09:39:52 +0500

    Eslint fixes

--- FCC/Set.js
+++ FCC/Set.js
@@ -1,66 +1,66 @@
 // Sets -> similar to arrays - to duplicate items - no particular order
 
-function mySet() {
+function MySet() {
   // this will hold the set
-  var collection = [];
+  const collection = [];
 
   // this method will check for the presence of an element and return true or false
-  this.has = function (element) {
-    return (collection.indexOf(element) !== 1);
-  }
+  this.has = function(element) {
+    return collection.indexOf(element) !== 1;
+  };
 
   // this method will return all values in the set
-  this.values = function () {
+  this.values = function() {
     return collection;
-  }
+  };
 
   // this method will add an element to the set
-  this.add = function (element) {
+  this.add = function(element) {
     if (!this.has(element)) {
       collection.push(element);
       return true;
     }
     return false;
-  }
+  };
 
   // this will remove an element from a set - In ES6 remove == delete
-  this.remove = function (element) {
+  this.remove = function(element) {
     if (this.has(element)) {
-      index = collection.indexOf(element);
+      const index = collection.indexOf(element);
       collection.splice(index, 1); // take out 1 element the starting from the defined index
       return true;
     }
     return false;
-  }
+  };
 
   // In ES6 - size is a property
-  this.size = function () {
+  this.size = function() {
     return collection.length;
-  }
+  };
 
   // this method will return the union of two sets
-  this.union = function (otherSet) {
-    var unionSet = new mySet();
-    var firstSet = this.values();
-    var secondSet = otherSet.values();
+  this.union = function(otherSet) {
+    const unionSet = new MySet();
+    const firstSet = this.values();
+    const secondSet = otherSet.values();
 
-    firstSet.forEach(function (e) {
+    firstSet.forEach(e => {
       unionSet.add(e);
     });
 
-    secondSet.forEach(function (e) {
+    secondSet.forEach(e => {
       unionSet.add(e);
     });
 
     return unionSet;
-  }
+  };
 
   // this method will return the intersection of two sets as a new set;
-  this.intersection = function (otherSet) {
-    var interSectionSet = new mySet();
-    var firstSet = this.values();
+  this.intersection = function(otherSet) {
+    const interSectionSet = new MySet();
+    const firstSet = this.values();
 
-    firstSet.forEach(function (e) {
+    firstSet.forEach(e => {
       if (otherSet.has(e)) {
         interSectionSet.add(e);
       }
@@ -70,31 +70,29 @@ function mySet() {
   };
 
   // this method will return the difference of two sets as a new set
-  this.difference = function (otherSet) {
-    var differenceSet = new mySet();
-    var firstSet = this.values();
+  this.difference = function(otherSet) {
+    const differenceSet = new MySet();
+    const firstSet = this.values();
 
-    firstSet.forEach(function (e) {
+    firstSet.forEach(e => {
       if (!otherSet.has(e)) {
         differenceSet.add(e);
       }
     });
 
     return differenceSet;
-  }
+  };
 
   // this method will test if the set is a subset of a different set
-  this.subset = function (otherSet) {
-    var firstSet = this.values();
-    return firstSet.every(function (value) {
-      return otherSet.has(value);
-    });
-  }
+  this.subset = function(otherSet) {
+    const firstSet = this.values();
+    return firstSet.every(value => otherSet.has(value));
+  };
 }
 
 // Examples uses of a Set
-var SetA = new mySet();
-var SetB = new mySet();
+const SetA = new MySet();
+const SetB = new MySet();
 
 setA.add('a');
 setA.add('b');
