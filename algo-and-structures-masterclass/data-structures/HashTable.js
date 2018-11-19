804dc2995030e63ef7dc14ff332d41d2df8fd649
husseyexplores <husseyexplores@gmail.com>
2018-11-19 21:39:39 +0500

    Define get, set, keys and values for HashTable

--- algo-and-structures-masterclass/data-structures/HashTable.js
+++ algo-and-structures-masterclass/data-structures/HashTable.js
@@ -12,4 +12,61 @@ class HashTable {
     }
     return total;
   }
+
+  set(key, val) {
+    const newArr = [key, val];
+    const idx = this._hash(key);
+    if (!this.keyMap[idx]) {
+      this.keyMap[idx] = [];
+    }
+    this.keyMap[idx].push(newArr);
+    return true;
+  }
+
+  get(key) {
+    const idx = this._hash(key);
+    if (this.keyMap[idx]) {
+      for (let i = 0; i < this.keyMap[idx].length; i++) {
+        if (this.keyMap[idx][i][0] === key) return this.keyMap[idx][i][1];
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const allKeys = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!allKeys.includes(this.keyMap[i][j][0])) {
+            allKeys.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return allKeys;
+  }
+
+  values() {
+    const allValues = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!allValues.includes(this.keyMap[i][j][1])) {
+            allValues.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return allValues;
+  }
 }
+
+const ht = new HashTable(13);
+ht.set('pink', '#pink');
+ht.set('cyan', '#cyan');
+ht.set('red', '#red');
+ht.set('redDup', '#red');
+ht.set('redDup2', '#red');
+ht.set('black', '#black');
+ht.set('black', '#blackDup');
