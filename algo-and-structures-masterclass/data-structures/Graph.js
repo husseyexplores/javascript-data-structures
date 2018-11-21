a9893d951179a97c4d775e3b0a1d4dca2f0aebaa
husseyexplores <husseyexplores@gmail.com>
2018-11-21 23:26:34 +0500

    Define addVertex method for Graph

--- algo-and-structures-masterclass/data-structures/Graph.js
+++ algo-and-structures-masterclass/data-structures/Graph.js
@@ -2,4 +2,14 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(value) {
+    this.adjacencyList[value] = [];
+  }
 }
+
+const g = new Graph();
+g.addVertex('Ontario');
+g.addVertex('Karachi');
+g.addVertex('Lahore');
+g.addVertex('Hunza');
