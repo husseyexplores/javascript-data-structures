ce84838e870c1871daa337a2d903c128cdde484b
husseyexplores <husseyexplores@gmail.com>
2018-11-23 23:44:00 +0500

    Define addEdge for Graph

--- algo-and-structures-masterclass/data-structures/Graph.js
+++ algo-and-structures-masterclass/data-structures/Graph.js
@@ -3,8 +3,20 @@ class Graph {
     this.adjacencyList = {};
   }
 
-  addVertex(value) {
-    this.adjacencyList[value] = [];
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vtx1, vtx2) {
+    if (!this.adjacencyList[vtx1].includes(vtx2)) {
+      this.adjacencyList[vtx1].push(vtx2);
+    }
+
+    if (!this.adjacencyList[vtx2].includes(vtx1)) {
+      this.adjacencyList[vtx2].push(vtx1);
+    }
   }
 }
 
