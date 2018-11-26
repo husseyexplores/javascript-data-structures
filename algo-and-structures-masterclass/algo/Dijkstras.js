504ca06df6efd96031cbab9de4768b6941cb0e68
husseyexplores <husseyexplores@gmail.com>
2018-11-26 18:20:30 +0500

    Now use PriorityQueue

--- algo-and-structures-masterclass/algo/Dijkstras.js
+++ algo-and-structures-masterclass/algo/Dijkstras.js
@@ -1,23 +1,87 @@
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  enq(val, prio) {
-    this.values.push({
-      val,
-      prio,
-    });
+  swap(array, idx1, idx2) {
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+  }
 
-    this.sort();
+  enq(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+    return this.values;
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    let parentIdx;
+    let parent;
+
+    while (idx > 0) {
+      parentIdx = Math.floor((idx - 1) / 2);
+      parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.swap(this.values, idx, parentIdx);
+      idx = parentIdx;
+    }
   }
 
   deq() {
-    return this.values.shift();
+    if (this.values.length === 0) return null;
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return min;
   }
 
-  sort() {
-    this.values.sort((a, b) => a.prio - b.prio);
+  bubbleDown() {
+    const { length } = this.values;
+    const element = this.values[0];
+
+    let idx = 0;
+    let leftChildIdx;
+    let rightChildIdx;
+    let leftChild;
+    let rightChild;
+
+    while (true) {
+      leftChildIdx = 2 * idx + 1;
+      rightChildIdx = 2 * idx + 2;
+      let swapIdx = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) swapIdx = leftChildIdx;
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swapIdx === null && rightChild.priority < element.priority) ||
+          (swapIdx !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swapIdx = rightChildIdx;
+        }
+      }
+
+      if (swapIdx === null) break;
+      this.values[idx] = this.values[swapIdx];
+      this.values[swapIdx] = element;
+      idx = swapIdx;
+    }
   }
 }
 
