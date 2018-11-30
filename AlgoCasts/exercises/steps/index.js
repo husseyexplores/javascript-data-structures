02f442908b4b7f4a602ba42a417c138d9f533bc8
husseyexplores <husseyexplores@gmail.com>
2018-11-30 18:40:25 +0500

    Add alternative iterative and recursive solution

--- AlgoCasts/exercises/steps/index.js
+++ AlgoCasts/exercises/steps/index.js
@@ -20,23 +20,65 @@
 /*
  * Solution #1 - own - O(n^2);
  */
-const steps = n => {
-  for (let i = 1; i <= n; i++) {
-    let result = '';
-    const totalSpaces = n - i;
-    const totalHashes = i;
+// const steps = n => {
+//   for (let i = 1; i <= n; i++) {
+//     let stair = '';
+//     const totalSpaces = n - i;
+//     const totalHashes = i;
 
-    // Build hashes
-    for (let j = 1; j <= totalHashes; j++) {
-      result += '#';
-    }
+//     // Build hashes
+//     for (let j = 1; j <= totalHashes; j++) {
+//       stair += '#';
+//     }
 
-    // Build Spaces
-    for (let j = 1; j <= totalSpaces; j++) {
-      result += ' ';
-    }
-    console.log(result);
+//     // Build Spaces
+//     for (let j = 1; j <= totalSpaces; j++) {
+//       stair += ' ';
+//     }
+//     console.log(stair);
+//   }
+// };
+
+/*
+ * Solution #2 - similar
+ */
+// const steps = n => {
+//   for (let i = 1; i <= n; i++) {
+//     let stair = '';
+
+//     for (let j = 1; j <= n; j++) {
+//       if (j <= i) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// };
+
+/*
+ * Solution #3 - recursion
+ */
+const steps = (n, row = 0, stair = '') => {
+  // last row - base case;
+  if (row === n) {
+    return;
   }
+
+  // stair is completed, print it and move to next row/stair
+  if (stair.length === n) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // building each stair;
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
 };
 
 module.exports = steps;
