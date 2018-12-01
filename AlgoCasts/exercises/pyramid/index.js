521eb62b0b92f3a47473f2555382271445912754
husseyexplores <husseyexplores@gmail.com>
2018-12-01 22:45:04 +0500

    Add own solution

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -13,7 +13,36 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+/*
+ * Solution #1 - own
+ */
+const pyramid = n => {
+  const maxChars = n * 2 - 1;
+  const midPoint = Math.floor(maxChars / 2);
+
+  for (let i = 0; i < n; i++) {
+    // create a empty array with filled with spaces - Each stair
+    const strArr = new Array(maxChars).fill(' ');
+
+    // hashes start from this idx
+    let hashIdx = midPoint - i;
+    // total hashes for each step;
+    const totalStepHashes = i * 2 + 1;
+
+    // replace the spaces with hashes
+    for (let j = 0; j < totalStepHashes; j++) {
+      strArr[hashIdx] = '#';
+      hashIdx++;
+    }
+
+    console.log(strArr.join(''));
+  }
+};
 
 module.exports = pyramid;
