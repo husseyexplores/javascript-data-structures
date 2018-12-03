bbeebd5d4f695be55af13bbb0d846dbca40435f5
husseyexplores <husseyexplores@gmail.com>
2018-12-03 20:20:18 +0500

    Matrix partial solution

--- AlgoCasts/exercises/matrix/index.js
+++ AlgoCasts/exercises/matrix/index.js
@@ -14,7 +14,33 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+/* eslint-disable */
+const matrix = num => {
+  const result = new Array(num).fill(new Array(num));
+  let startRowIdx = 0;
+  let startColIdx = 0;
+  let endRowIdx = num - 1;
+  let endColIdx = num - 1;
 
-function matrix(n) {}
+  let counter = 1;
 
+  while (startColIdx <= endColIdx && startRowIdx <= endRowIdx) {
+    for (let i = startColIdx; i <= endColIdx; i++) {
+      result[startRowIdx][i] = counter;
+      counter++;
+    }
+
+    startRowIdx++;
+
+    for (let i = startRowIdx; i <= endRowIdx; i++) {
+      result[i][endColIdx] = counter;
+      counter++;
+    }
+
+    endRowIdx--;
+  }
+
+  return result;
+};
+matrix(3);
 module.exports = matrix;
