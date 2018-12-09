9d747ab7b95f849b0b9d1ef8545822e212e96ac1
husseyexplores <husseyexplores@gmail.com>
2018-12-09 23:07:33 +0500

    Iterative and recursize solution

--- AlgoCasts/exercises/fib/index.js
+++ AlgoCasts/exercises/fib/index.js
@@ -8,9 +8,9 @@
 // Example:
 //   fib(4) === 3
 
-// /*
-//  * Solution #1 - own;
-//  */
+/*
+ * Solution #1 - own;
+ */
 // const fib = n => {
 //   if (n === 1) return 1;
 //   let result = 0;
@@ -25,14 +25,35 @@
 // };
 
 /*
- * Solution #1 - own recursive;
+ * Solution #2 - own recursive;
+ */
+// const fib = (n, prevNum = 0, curNum = 1, series = [0, 1]) => {
+//   if (n === series.length - 1) return series[n];
+//   const newPrevNum = curNum;
+//   const newCurNum = prevNum + curNum;
+//   series.push(newCurNum);
+//   return fib(n, newPrevNum, newCurNum, series);
+// };
+
+/*
+ * Solution #3 - Iterative
+ */
+// const fib = n => {
+//   const series = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = series[i - 1];
+//     const b = series[i - 2];
+//     series.push(a + b);
+//   }
+//   return series[n];
+// };
+
+/*
+ * Solution #4 - Recursive;
  */
 const fib = (n, prevNum = 0, curNum = 1, series = [0, 1]) => {
-  if (n === series.length - 1) return series[n];
-  const newPrevNum = curNum;
-  const newCurNum = prevNum + curNum;
-  series.push(newCurNum);
-  return fib(n, newPrevNum, newCurNum, series);
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
 };
 
 module.exports = fib;
